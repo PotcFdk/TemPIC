69ca4ee7d22358e6b30ecdf8d2dece91cff8201b
PotcFdk <PotcFdk@gmx.net>
2014-07-03 16:44:39 +0200

    White-spacing normalization.

--- web/index.php
+++ web/index.php
@@ -4,15 +4,15 @@ session_start();
 ?>
 <!doctype html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>TemPIC</title>
+	<head>
+		<meta charset="utf-8">
+		<title>TemPIC</title>
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link href="css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
 
-        <script src="js/jquery-2.1.0.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery-2.1.0.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
 	<script src="js/fileinput.min.js"></script>
 
 	<script>
@@ -23,38 +23,38 @@ session_start();
 		});
 	</script>
 
-        <style>
+		<style>
 			.page-header h1 {
 				cursor: default;
 			}
 			
-        	form {
-        		margin-bottom: 10px;
-        	}
+			form {
+				margin-bottom: 10px;
+			}
 			
-        	.panel-body a {
-        		text-align: center;
-        	}
+			.panel-body a {
+				text-align: center;
+			}
 
-        	.panel-body img {
-        		margin: 0 auto 10px;
-        	}
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-                        <h1>TemPIC</h1>
-                    </div>
+			.panel-body img {
+				margin: 0 auto 10px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="page-header">
+						<h1>TemPIC</h1>
+					</div>
 
-                    <form class="form-horizontal" method="post" action="upload.php" enctype="multipart/form-data">
-                        <div class="form-group">
-                            <label for="file" class="col-md-1 control-label">Files</label>
-                            <div class="col-md-7">
-                                <input class="file" type="file" name="file[]" id="file" multiple="multiple">
-                            </div>
+					<form class="form-horizontal" method="post" action="upload.php" enctype="multipart/form-data">
+						<div class="form-group">
+							<label for="file" class="col-md-1 control-label">Files</label>
+							<div class="col-md-7">
+								<input class="file" type="file" name="file[]" id="file" multiple="multiple">
+							</div>
 							<div class="col-md-2">
 								<select class="form-control" name="lifetime">
 								<?php foreach ($LIFETIMES as $id => $data) : ?>
@@ -62,47 +62,47 @@ session_start();
 								<?php endforeach; ?>
 								</select>
 							</div>
-                        </div>
-                    </form>
+						</div>
+					</form>
 
-                    <?php if (!empty($_SESSION['files'])) : ?>
-                        <?php $count = 0; ?>
-                        <?php foreach ($_SESSION['files'] as $name => $file) : ?>
-                            <?php if ($count % 3 == 0) : ?><div class="row"><?php endif; ?>
-                                <div class="col-md-4">
-                                	<?php if (!empty($file['error'])) : ?>
+					<?php if (!empty($_SESSION['files'])) : ?>
+						<?php $count = 0; ?>
+						<?php foreach ($_SESSION['files'] as $name => $file) : ?>
+							<?php if ($count % 3 == 0) : ?><div class="row"><?php endif; ?>
+								<div class="col-md-4">
+									<?php if (!empty($file['error'])) : ?>
 		                                <div class="alert alert-danger alert-dismissable">
 		                                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 		                                    Error uploading "<?php echo $name; ?>": <?php echo $file['error']; ?>
 		                                </div>
 		                            <?php else: ?>
-                                        <div class="panel panel-default">
-                                            <div class="panel-body">
-                                            	<a href="<?php echo $file['link']; ?>">
+										<div class="panel panel-default">
+											<div class="panel-body">
+												<a href="<?php echo $file['link']; ?>">
 	                                                <?php if ($file['image']) : ?>
 	                                                    <img src="<?php echo $file['link']; ?>" alt="Uploaded Image" class="thumbnail img-responsive">
 	                                                <?php else: ?>
 	                                                	<?php $image = $URL_BASE . '/img/filetypes/' . (file_exists('img/filetypes/' . $file['extension'] . '.png') ? $file['extension'] : '_blank') . '.png'; ?>
-                                                		<img src="<?php echo $image; ?>" alt="Uploaded File" class="img-responsive">
+														<img src="<?php echo $image; ?>" alt="Uploaded File" class="img-responsive">
 	                                                <?php endif; ?>
 
 	                                                <p><?php echo htmlspecialchars($name); ?></p>
-                                                </a>
-                                            </div>
-                                        </div>
-                                    <?php endif; ?>
-                                </div>
+												</a>
+											</div>
+										</div>
+									<?php endif; ?>
+								</div>
 
-                            <?php if ($count % 3 == 2) : ?></div><?php endif; ?>
-                            <?php $count++; ?>
-                        <?php endforeach; ?>
+							<?php if ($count % 3 == 2) : ?></div><?php endif; ?>
+							<?php $count++; ?>
+						<?php endforeach; ?>
 
-                        <?php if ($count % 3 != 0) : ?></div><?php endif; ?>
-                    <?php endif; ?>
-                </div>
-            </div>
-        </div>
-    </body>
+						<?php if ($count % 3 != 0) : ?></div><?php endif; ?>
+					<?php endif; ?>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
 
 <?php unset($_SESSION['files']); ?>
--- web/upload.php
+++ web/upload.php
@@ -2,79 +2,79 @@
 include_once('config.php');
 
 function isImage($file) {
-    $finfo = finfo_open(FILEINFO_MIME_TYPE);
-    $mime = finfo_file($finfo, $file);
-    finfo_close($finfo);
+	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+	$mime = finfo_file($finfo, $file);
+	finfo_close($finfo);
 
-    return ($mime == 'image/gif')
-        || ($mime == 'image/jpeg')
-        || ($mime == 'image/jpg')
-        || ($mime == 'image/pjpeg')
-        || ($mime == 'image/x-png')
-        || ($mime == 'image/png')
+	return ($mime == 'image/gif')
+		|| ($mime == 'image/jpeg')
+		|| ($mime == 'image/jpg')
+		|| ($mime == 'image/pjpeg')
+		|| ($mime == 'image/x-png')
+		|| ($mime == 'image/png')
 		|| ($mime == 'image/svg+xml');
 }
 
 function rearrange($arr) {
-    foreach ($arr as $key => $all) {
-        foreach($all as $i => $val) {
-            $new[$i][$key] = $val;    
-        }    
-    }
-    return $new;
+	foreach ($arr as $key => $all) {
+		foreach($all as $i => $val) {
+			$new[$i][$key] = $val;    
+		}    
+	}
+	return $new;
 }
 
 if (is_uploaded_file($_FILES['file']['tmp_name'][0])) {
-    session_start();
-    $files = array();
+	session_start();
+	$files = array();
 
-    // Because PHP structures the array in a retarded format
-    $_FILES['file'] = rearrange($_FILES['file']);
+	// Because PHP structures the array in a retarded format
+	$_FILES['file'] = rearrange($_FILES['file']);
 
-    foreach ($_FILES['file'] as $file) {
-        $files[$file['name']] = array();
-        $lifetime = "test";
+	foreach ($_FILES['file'] as $file) {
+		$files[$file['name']] = array();
+		$lifetime = "test";
 
-        if ($file['size'] < 2000000) {
-            $extension = pathinfo($file['name'], PATHINFO_EXTENSION);
-            $lifetime = $_POST['lifetime'];
+		if ($file['size'] < 2000000) {
+			$extension = pathinfo($file['name'], PATHINFO_EXTENSION);
+			$lifetime = $_POST['lifetime'];
 
-            if (in_array($extension, $DISALLOWED_EXTS)) {
-                $files[$file['name']]['error'] = 'Disallowed file type!';
-            } elseif ($file['error'] > 0) {
-                $files[$file['name']]['error'] = 'Return Code: ' . $file['error'];
-            } elseif (!isset($lifetime) || !array_key_exists($lifetime, $LIFETIMES)) {
-                $files[$file['name']]['error'] = 'Invalid or no file lifetime specified.';
-            } else {
-                $path_destination = $PATH_UPLOAD . '/' . $lifetime;
-                
-                if (!file_exists($path_destination)) {
-                    mkdir($path_destination, 0775);
+			if (in_array($extension, $DISALLOWED_EXTS)) {
+				$files[$file['name']]['error'] = 'Disallowed file type!';
+			} elseif ($file['error'] > 0) {
+				$files[$file['name']]['error'] = 'Return Code: ' . $file['error'];
+			} elseif (!isset($lifetime) || !array_key_exists($lifetime, $LIFETIMES)) {
+				$files[$file['name']]['error'] = 'Invalid or no file lifetime specified.';
+			} else {
+				$path_destination = $PATH_UPLOAD . '/' . $lifetime;
+				
+				if (!file_exists($path_destination)) {
+					mkdir($path_destination, 0775);
 					chmod($path_destination, 0775);
 				}
-                
-                $name = time() . '_' . rand(100000000, 999999999) . '_' . $file['name'];
-                $path = $path_destination . '/' . $name;
-                
-                if (file_exists($path)) {
-                    $files[$file['name']]['error'] = $name . ' already exists.';
-                } else {
-                    move_uploaded_file($file['tmp_name'], $path);
+				
+				$name = time() . '_' . rand(100000000, 999999999) . '_' . $file['name'];
+				$path = $path_destination . '/' . $name;
+				
+				if (file_exists($path)) {
+					$files[$file['name']]['error'] = $name . ' already exists.';
+				} else {
+					move_uploaded_file($file['tmp_name'], $path);
 					chmod($path, 0664);
 
-                    $link = $URL_BASE . '/' . $path;
+					$link = $URL_BASE . '/' . $path;
 
-                    $files[$file['name']]['link'] = $link;
-                    $files[$file['name']]['image'] = isImage($path);
-                    $files[$file['name']]['extension'] = $extension;
-                }
-            }
-        } else {
-            $files[$file['name']]['error'] = 'File too large!';
-        }
-    }
+					$files[$file['name']]['link'] = $link;
+					$files[$file['name']]['image'] = isImage($path);
+					$files[$file['name']]['extension'] = $extension;
+				}
+			}
+		} else {
+			$files[$file['name']]['error'] = 'File too large!';
+		}
+	}
 
-    $_SESSION['files'] = $files;
+	$_SESSION['files'] = $files;
 }
 
 header('Location: ' . $URL_BASE);
