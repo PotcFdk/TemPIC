891ecb83d7c77516504cdaffb84bb71b2326cd63
PotcFdk <PotcFdk@gmx.net>
2014-09-12 19:08:04 +0200

    Added missing include call for helpers.php

--- scripts/cleaner.php
+++ scripts/cleaner.php
@@ -16,6 +16,7 @@
 	require_once ('config.php');
 	require_once ($PATH_TEMPIC . '/config.php');
 	require_once ($PATH_INCLUDES . '/baseconfig.php');
+	require_once ($PATH_INCLUDES . '/helpers.php');
 	
 	$time  = time ();
 
