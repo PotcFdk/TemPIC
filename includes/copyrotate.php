ea7763896732bfbaf0ea71c8e217bdd8522025a7
PotcFdk <PotcFdk@gmx.net>
2014-09-13 15:10:26 +0200

    Generating full url in copyrotate.php, if possible.

--- includes/copyrotate.php
+++ includes/copyrotate.php
@@ -1,6 +1,6 @@
 <div class="copyrotate">
 	<a href="https://github.com/PotcFdk/TemPIC">
-		<img src="img/copyrotate.svg" alt="" width="20" height="20">
+		<img src="<?php if (!empty($URL_BASE)) { echo($URL_BASE.'/'); } ?>img/copyrotate.svg" alt="" width="20" height="20">
 		<div class="text">Powered by TemPIC<br />
 		<div class="rotate">&copy;</div> PotcFdk, ukgamer, 2014<br />
 		Visit this project on GitHub!</div>
