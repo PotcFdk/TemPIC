5079b94ce73bee7ab89a6027e14991d2b1b5d9bc
PotcFdk <PotcFdk@gmx.net>
2014-12-03 22:26:47 +0100

    Added upload speed (float) rounding.

--- web/index.php
+++ web/index.php
@@ -180,7 +180,7 @@ session_start();
 					$('#progressbar').attr('value', percentComplete.toString());
 					$('#progresstext').html("Uploading: " + percentCompleteStr + "<br />"
 						+ humanFileSize(evt.loaded) + " / " + humanFileSize(evt.total) + " total"
-							+ "@ " + humanFileSize(speed) + " per second<br />"
+							+ "@ " + humanFileSize(speed.toFixed(2)) + " per second<br />"
 						+ "Elapsed: " + millisecondsToStr(duration) + "<br />"
 						+ 'ETA: ' + millisecondsToStr((evt.total - evt.loaded)/speed));
 					document.title = "<?php echo $INSTANCE_NAME; ?> - " + percentCompleteStr + " (uploading)";
