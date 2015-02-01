609fb0a2d6857c0512f0b8b4248ff4a0e0bd4a03
PotcFdk <PotcFdk@gmx.net>
2015-02-01 23:00:01 +0100

    Fixed a possible PHP error in mb_pathinfo.

--- web/upload.php
+++ web/upload.php
@@ -46,10 +46,10 @@ function isImage($file) {
 
 function mb_pathinfo($filepath) {
 	preg_match ('%^(.*?)[\\\\/]*(([^/\\\\]*?)(\.([^\.\\\\/]+?)|))[\\\\/\.]*$%im', $filepath, $m);
-	if ($m[1]) $ret['dirname']   = $m[1];
-	if ($m[2]) $ret['basename']  = $m[2];
-	if ($m[5]) $ret['extension'] = $m[5];
-	if ($m[3]) $ret['filename']  = $m[3];
+	if (!empty($m[1])) $ret['dirname']   = $m[1];
+	if (!empty($m[2])) $ret['basename']  = $m[2];
+	if (!empty($m[5])) $ret['extension'] = $m[5];
+	if (!empty($m[3])) $ret['filename']  = $m[3];
 	return $ret;
 }
 
