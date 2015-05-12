db24a26d011969b5a77d6d074c0acddf4c16a0c5
PotcFdk <PotcFdk@gmx.net>
2015-05-12 21:06:14 +0200

    Prevent PHP error when the 'album' GET variable is an array.

--- web/index_nojs.php
+++ web/index_nojs.php
@@ -52,7 +52,7 @@ session_start();
 			$remaining_time = $LIFETIMES[$_SESSION['album_lifetime']]['time']*60;
 	}
 
-	if (empty($files) && isset($_GET['album'])) {
+	if (empty($files) && is_string($_GET['album'])) {
 		$album_id = strip_album_id($_GET['album']);
 		if (!empty($album_id)) {
 			$_a = explode(":", $album_id, 2);
