cb77207c367f51262f09b109024ed06de918c3ef
KeyLimePie <v.asakura@ymail.com>
2015-05-20 19:22:06 +0200

    this! UploadManager license added

--- web/js/uploadmanager.js
+++ web/js/uploadmanager.js
@@ -1,3 +1,19 @@
+/*
+	UploadManager for TemPIC - Copyright  2015; KeyLimePie (GitHub: TheKeyLimePie)
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+	
+	http://www.apache.org/licenses/LICENSE-2.0
+	
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 function UploadManager(baseurl, uploadProgress, uploadComplete, uploadFailed, uploadCanceled)
 {
 	this.UPLOADPATH = baseurl.concat("/upload.php");
@@ -63,7 +79,7 @@ UploadManager.prototype.makePOSTData = function()
 	
 	for(var x = 0; x < this.files.length; x++)
 	{
-		fd.append("file[]", files[x]);
+		fd.append("file[]", this.files[x]);
 	}
 	
 	return fd;
