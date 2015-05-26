cd97764254c6523d7481ed6d432a981eeaa47882
PotcFdk <PotcFdk@gmx.net>
2015-05-26 16:03:49 +0200

    Basic deserializing.

--- web/api.php
+++ web/api.php
@@ -15,6 +15,11 @@
 	limitations under the License.
 */
 
+require_once('../includes/baseconfig.php');
+@include_once('config.php');
+
+require_once('../includes/helpers.php');
+
 define ('STATUS_SUCCESS', 'success');
 define ('STATUS_FAIL',    'fail');
 define ('STATUS_ERROR',   'error');
@@ -65,20 +70,35 @@ reply();
 
 // API handlers
 
-function API_V1_BAD_REQUEST()
+function API_V1_BAD_REQUEST ($reason = NULL)
 {
 	global $resp;
 	http_response_code (400); // Bad Request
 	$resp['status'] = STATUS_FAIL;
-	$resp['data'] = array ('error' => 'Invalid action.');
+	$resp['data'] = array ('error' => !empty ($reason) ? $reason : 'Invalid action.');
 }
 
 function API_V1_ALBUM_INFO ($album_id)
 {
-	global $resp;
-	http_response_code (501); // Not Implemented
-	$resp['status'] = STATUS_FAIL;
-	$resp['data'] = array ('error' => 'Album information unavailable.');
+	global $LIFETIMES, $PATH_ALBUM, $resp;
+	//http_response_code (501); // Not Implemented
+	
+	$album_id = strip_album_id($album_id);
+	if (empty($album_id)) return API_V1_BAD_REQUEST ("Invalid album ID");
+	
+	$_a = explode (":", $album_id, 2);
+	if (!empty ($_a[0]))
+		$album_lifetime = $_a[0];
+	if (!empty ($_a[1]))
+		$album_hash = $_a[1];
+	
+	if (!empty ($LIFETIMES[$album_lifetime]) && file_exists ($PATH_ALBUM.'/'.$album_lifetime.'/'.$album_hash.'.txt'))
+		$album_data = unserialize(file_get_contents($PATH_ALBUM.'/'.$album_lifetime.'/'.$album_hash.'.txt'));
+	else
+		return API_V1_BAD_REQUEST ("Album can not be found");
+	
+	$resp['status'] = STATUS_SUCCESS;
+	$resp['data'] = array ('rawdata', $album_data);
 }
 
 function API_V1 (&$chunks)
