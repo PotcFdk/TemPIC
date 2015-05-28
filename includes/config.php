6e628ae1266bff91c130861bc8d46ad67e276118
PotcFdk <PotcFdk@gmx.net>
2015-05-29 01:15:22 +0200

    Added the internal configuration defaults file.

---
+++ includes/config.php
@@ -0,0 +1,31 @@
+<?php
+	/*
+		TemPIC - Copyright (c) PotcFdk, 2014 - 2015
+
+		Licensed under the Apache License, Version 2.0 (the "License");
+		you may not use this file except in compliance with the License.
+		You may obtain a copy of the License at
+		
+		http://www.apache.org/licenses/LICENSE-2.0
+		
+		Unless required by applicable law or agreed to in writing, software
+		distributed under the License is distributed on an "AS IS" BASIS,
+		WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+		See the License for the specific language governing permissions and
+		limitations under the License.
+	*/
+
+	// Internal configuration.
+	// Don't edit this file.
+	// You can override these settings in the config.php in your web directory.
+
+	$PATH_ALBUM = 'album';
+	$PATH_UPLOAD = 'upload';
+	
+	$PATH_JOBQUEUE = '../internal/thumbnail_queue';
+	
+	$THUMBNAIL_PREFIX = '_thumb_';
+	
+	$MAX_ALBUM_NAME_LENGTH = 150;
+	$MAX_ALBUM_DESCRIPTION_LENGTH = 5000;
+?>
