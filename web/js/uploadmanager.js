a9dbd2248778e0758a6f8d537bbe61d2172fd7d3
PotcFdk <PotcFdk@gmx.net>
2015-09-05 18:23:40 +0200

    Only check for duplicate filenames; ignore metadata.

--- web/js/uploadmanager.js
+++ web/js/uploadmanager.js
@@ -68,14 +68,14 @@ UploadManager.prototype.addFile = function(file)
 		var duplicate = false;
 		for(var x = 0; x < this.getFiles().length; x++)
 		{
-			if(file.size == this.getFiles()[x].size && file.lastModified == this.getFiles()[x].lastModified && file.type == this.getFiles()[x].type)
+			if(file.name == this.getFiles()[x].name)
 			{
-				duplicate = true;
+				duplicate = this.getFiles()[x].name;
 				break;
 			}
 		}
 		if(duplicate)
-			warn("Duplicate files are not allowed.");
+			warn("Duplicate filename: " + duplicate);
 		else
 			this.files.push(file);
 	}	
