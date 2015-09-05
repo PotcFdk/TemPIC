89ada5a918a3c815bdbc1f4dceb582f1df11db53
PotcFdk <PotcFdk@gmx.net>
2015-09-05 19:15:14 +0200

    Improve the scaling of the QR-Code modal.

--- web/index.php
+++ web/index.php
@@ -303,7 +303,7 @@ require_once('../includes/qrcode-interface.php');
 						<?php endif; ?>
 
 						<div class="modal fade" id="modal_qrcode" tabindex="-1" role="dialog" aria-labelledby="label_modal_qrcode">
-							<div class="modal-dialog" role="document">
+							<div class="modal-dialog modal-qrcode" role="document">
 								<div class="modal-content">
 									<div class="modal-header">
 										<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
