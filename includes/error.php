15af8710ae0b6d1ed9c86db3f591efa348dc52de
PotcFdk <PotcFdk@gmx.net>
2019-10-22 23:59:53 +0200

    Update copyright years.

--- includes/error.php
+++ includes/error.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <!--
-	TemPIC - Copyright (c) PotcFdk, 2014 - 2018
+	TemPIC - Copyright (c) PotcFdk, 2014 - 2019
 
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
